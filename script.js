fa699d2930b3008514aa01024fadae702932c023
rdsalvesPUC <s.rodrigo5@pucpr.edu.br>
2024-06-03 12:46:47 -0300

    filtros, busca, cards e todos cinzas resolvidos

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 fetch("footer.html")
-                .then((response) => response.text())
-                .then((data) => {
-                    document.getElementById("footer-container").innerHTML = data;
-                });
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("footer-container").innerHTML = data;
+    });
 
 fetch("header.html")
     .then((response) => response.text())
     .then((data) => {
         document.getElementById("header-container").innerHTML = data;
     });
+
+fetch("search-bar.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("search").innerHTML = data;
+    });
+
+fetch("filters.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("filters").innerHTML = data;
+    });
+
+function loadCard(container) {
+    fetch("card.html")
+        .then((response) => response.text())
+        .then((data) => {
+            container.innerHTML = data;
+        });
+}
+
+document.querySelectorAll('.card-container').forEach(container => {
+    loadCard(container);
+});
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const passwordField = document.getElementById('Senha');
